8ae480da0df82faa92dd252b7dda9d443bd32344
MINSOO KANG <stengine2@gmail.com>
2020-08-25 13:21:53 +0900

    Data manipulation
    I renamed the file and added frequency manipulation methods

--- Data/SoundPCM/PCMProcessing.py
+++ Data/SoundPCM/PCMProcessing.py
@@ -1,4 +1,5 @@
 from os import listdir
+from numpy import average
 import csv
 import random
 	
@@ -24,7 +25,7 @@ def LoadFiles(filePath = None):
 	return(filesDict)
 
 def TruncateToMinLength(dataCollection):
-
+	"""This method matches the length of the data by cutting off the tails of longer files"""
 	# Get minimum length and file name of it
 	minLength = 9999999
 	fileName = ""
@@ -60,27 +61,76 @@ def SaveData(data, filePath = None):
 	print("Saved the truncated and combined file")
 
 def MatchFrequency(data, originalF, targetF):
-	pass
+	if originalF > targetF:
+		return DecreaseFrequency(data, originalF, targetF)
+	elif originalF < targetF:
+		return IncreaseFrequency(data, originalF, targetF)
+	else:
+		return data
 
 def IncreaseFrequency(data, originalF, targetF):
-	pass
+	"""This method uses interpolation to fill in the gaps"""
+	baseStep = targetF // originalF
+	randomAddPossibility = targetF % originalF
+	returnData = []
+	index = 0
+	endOfList = False
+	randAdd = [1 for i in range(randomAddPossibility)] + [0 for i in range(originalF - randomAddPossibility)]
+	while not endOfList:
+		random.shuffle(randAdd)
+		for randomArrayIndex in range(originalF):
+			try:
+				returnData += interpolate(data[index], data[index + 1], baseStep + randAdd[randomArrayIndex])
+			except IndexError:
+				endOfList = True
+				break
+			index += 1
+	return(returnData)
+			
 
-def DecreaseFrequency(data, originalF, targetF):
+def interpolate(point1, point2, numberOfPoints):
+	"""<numberOfPoints> should be greater or equal to 1.
+	<numberOfPoints> is number of points from point1 until point2."""
+	if numberOfPoints == 1:
+		return([point1])
+	interval = (point2 - point1) / numberOfPoints
+	return([point1 + i * interval for i in range(numberOfPoints)])
+	
+
+def DecreaseFrequency(data, originalF, targetF, avgOption = True):
+	"""Decrease frequency by sampling from original data.
+	This method uses psuedo random distribution to ensure it has rather uniform smapling rate match.
+	With avgOption on(True), the sampling will use the average of the missed datapoints.
+	If the option is False, it will sample from a single point."""
 	baseStep = originalF // targetF
 	randomAddPossibility = originalF % targetF
 	returnData = []
-	endOfList = False
 	index = 0
-	while not endOfList:
-		try:
-			returnData.append(data[index])
-		except IndexError:
-			endOfList = True			
-		if random.randrange(0, targetF) >= randomAddPossibility:
-			randomAddition = 0
-		else:
-			randomAddition = 1
-		index = index + baseStep + randomAddition
+	endOfList = False
+	randAdd = list([1 for i in range(randomAddPossibility)] + [0 for i in range(targetF-randomAddPossibility)])
+	if avgOption:
+		prev = 0
+		while not endOfList:
+			random.shuffle(randAdd)
+			for randomArrayIndex in range(targetF):
+				slice = data[prev:index]
+				if not slice == []:
+					returnData.append(average(slice))
+				else:
+					endOfList = True
+					break
+				prev = index
+				index += baseStep + randAdd[randomArrayIndex]
+	else:
+		while not endOfList:
+			random.shuffle(randAdd)
+			for randomArrayIndex in range(targetF):
+				try:
+					returnData.append(data[index])
+				except IndexError:
+					endOfList = True
+					break
+				index += baseStep + randAdd[randomArrayIndex]
 	return returnData
 	
 if (__name__ == "__main__"):
